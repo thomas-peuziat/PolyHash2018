6afb255d8ceb26ba146af7557c7619ed9885e0c4
Thomas PEUZIAT <thomas.peuziat@etu.univ-nantes.fr>
2018-10-11 15:09:40 +0200

    Ajout description parser

---
+++ utils/parser.py
@@ -0,0 +1,13 @@
+# PARSER
+# ------
+#
+# Passerelle entre les fichiers de donnees (dans polyhash/data/) et le reste du programme.
+# Le parser peut traiter des fichiers formatés en entrée et va fournir des fichiers formatés en
+# sorti, voir https://hashcode.withgoogle.com/2018/tasks/hashcode2018_final_task.pdf categorie "Input data set"
+# et "Submissions"
+#
+
+
+
+
+
