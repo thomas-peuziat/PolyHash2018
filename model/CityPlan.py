2b3503924774b055e2ca823f71809f2316529cbc
Gauducheau Clément <clement.gauducheau@etu.univ-nantes.fr>
2018-10-12 14:05:34 +0200

    création des classes

---
+++ model/CityPlan.py
@@ -0,0 +1,21 @@
+
+class CityPlan():
+
+	def __init__(self, matrix, name, dist):
+		self.matrix = matrix
+		self.nameProject = name
+		self.distManhattanMax = dist
+		self.nbProjectPlaced = 0
+	
+	def addTo(building, posX, posY):
+		if(building.isPlaceable()):
+			lignes, colonnes = building.matrix.shape
+
+			#on parcourt a partir de la posY definie jusqu'a 
+			#la posY+le nombre de lignes du batiments
+			for numLignes in range(posY,posY+lignes):
+				#on parcourt a partir de la posX definie jusqu'a 
+				#la posX+ le nombre de colonnes du batiments
+				for numColonnes in range(posX,posX+colonnes):
+					#TODO ajouter dans la ville
+					self.matrix[numLignes,numColonnes]=building
