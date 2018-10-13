5bd98d7a5093c1969bb951d39325bfa4ee0ceabf
Gauducheau Clément <clement.gauducheau@etu.univ-nantes.fr>
2018-10-13 11:08:45 +0200

    ajout heritage et ajout batiment dans ville

--- model/Building.py
+++ model/Building.py
@@ -12,7 +12,6 @@ class Building():
 	#acces aux valeurs de la matrice la ligne puis la colonne
 	#X=colonne / Y = ligne
 	def isPlaceable(self, city, posX, posY):
-		print("\n\n\n")
 		try:
 			if(city.matrix[posY,posX]=='#'):
 				return False
@@ -25,20 +24,53 @@ class Building():
 					#on parcourt a partir de la posX definie jusqu'a 
 					#la posX+ le nombre de colonnes du batiments
 					for numColonnes in range(posX,posX+colonnes):
-						print(city.matrix[numLignes,numColonnes])
 						if(city.matrix[numLignes,numColonnes]=='#'):
 							return False
 				return True
 		except:
 			return False
-		
-		
+
+#fin classe Building
+
+class Residential(Building):
+
+	def __init__(self, matrix, capacity):
+		Building.__init__(self, matrix)
+		self.capacity = capacity
+
+"""Exemple de methode de la sous classe
+	def afficherMatrix(self):
+		print(self.matrix)		
+"""
+#fin classe Residential
+
+class Utility(Building):
+	
+	def __init__(self, matrix, Type):
+		Building.__init__(self, matrix)
+		self.Type = Type
+
+
+#fin classe Utility
+
+
+###### MAIN pour les tests######
+
+#definition de la ville
 matriceVille=np.array([['.','.','.'],['#','#','#'],['.','.','.'],['#','#','#'],['.','.','.'],['#','#','#'],['.','.','.'],['.','.','.'],['.','.','.'],['.','.','.']])
 c=CityPlan.CityPlan(matriceVille,"Lyon",4)
 print(c.matrix)
 
+#definition des batiments
 matBuild=np.array([['.','#','#'],['#','#','#'],['#','.','#']])
-b=Building(matBuild)
+resi=Residential(matBuild,25)
 
-print(b.isPlaceable(c,0,2))
-print(b.matrix)
+#print("residential placeable en 0,2 ? ",resi.isPlaceable(c,0,6))
+print("\n")
+utili=Utility(matBuild,5)
+print(utili.matrix)
+print("\n")
+
+#ajout d'un utilitaire dans la ville
+c.add(utili, 0, 2)
+print(c.matrix)
--- model/CityPlan.py
+++ model/CityPlan.py
@@ -7,15 +7,20 @@ class CityPlan():
 		self.distManhattanMax = dist
 		self.nbProjectPlaced = 0
 	
-	def addTo(building, posX, posY):
-		if(building.isPlaceable()):
+	def add(self, building, posX, posY):
+		if(building.isPlaceable(self, posX, posY)):
 			lignes, colonnes = building.matrix.shape
-
+					
+			buildingLigne=0
+			
 			#on parcourt a partir de la posY definie jusqu'a 
 			#la posY+le nombre de lignes du batiments
 			for numLignes in range(posY,posY+lignes):
+				buildingColonne=0
 				#on parcourt a partir de la posX definie jusqu'a 
 				#la posX+ le nombre de colonnes du batiments
 				for numColonnes in range(posX,posX+colonnes):
-					#TODO ajouter dans la ville
-					self.matrix[numLignes,numColonnes]=building
+					self.matrix[numLignes,numColonnes]=building.matrix[buildingLigne,buildingColonne]
+					buildingColonne+=1
+				buildingLigne+=1
+				
