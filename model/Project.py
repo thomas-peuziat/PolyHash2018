025d5dc0ea99c6af240043d8164ebd64e030545b
Gauducheau Clément <clement.gauducheau@etu.univ-nantes.fr>
2018-10-22 22:42:15 +0200

    ajout fonction pour calculer les positions respectant la distance de manhattan d'un batiment

--- model/Project.py
+++ model/Project.py
@@ -43,3 +43,59 @@ class Project:
                 return True
         except IndexError:
             return False
+
+    #matrix = city_plan
+    def get_manhattan_surface(self, dist, matrix):
+        #Exemple avec utili
+        #list_coordinates_full = [(3,2),(4,1),(4,2),(5,1),(5,3)]
+
+        #Exemple avec resi
+        list_coordinates_full = [(1,4),(2,3),(2,4),(3,3),(3,5)]
+        list_coordinates_empty = []
+        nb_row_matrix, nb_column_matrix = matrix.shape
+
+        for i in range (dist):
+            for case in list_coordinates_full:
+                if(0<=case[0]+(dist-i)<=nb_row_matrix and matrix[case[0]+(dist-i),case[1]]=='.' and (case[0]+(dist-i),case[1]) not in list_coordinates_empty):
+                    list_coordinates_empty.append((case[0]+(dist-i),case[1]))
+                    matrix[case[0]+(dist-i)][case[1]]='#'
+
+                if (0<=case[0]-(dist-i)<=nb_row_matrix and matrix[case[0]-(dist-i), case[1]]=='.' and (case[0]-(dist-i),case[1]) not in list_coordinates_empty):
+                    list_coordinates_empty.append((case[0]-(dist-i),case[1]))
+                    matrix[case[0] - (dist - i)][case[1]] = '#'
+
+                if (0<=case[1]+(dist-i)<=nb_column_matrix and matrix[case[0], case[1]+(dist-i)]=='.' and (case[0],case[1]+(dist-i)) not in list_coordinates_empty):
+                    list_coordinates_empty.append((case[0],case[1]+(dist-i)))
+                    matrix[case[0]][case[1]+(dist-i)] = '#'
+
+                if (0<=case[1]-(dist-i)<=nb_column_matrix and matrix[case[0], case[1]-(dist-i)]=='.' and (case[0],case[1]-(dist-i)) not in list_coordinates_empty):
+                    list_coordinates_empty.append((case[0],case[1]-(dist-i)))
+                    matrix[case[0]][case[1]-(dist-i)] = '#'
+
+                if(i>0):
+                    diff=dist-i
+                    #bas droite
+                    if(0<=case[0]+i<=nb_row_matrix and 0<=case[1]+diff<=nb_column_matrix):
+                        if(matrix[case[0]+i, case[1]+diff]=='.'):
+                            list_coordinates_empty.append((case[0]+i, case[1]+diff))
+                            matrix[case[0] + i, case[1] + diff]='#'
+
+                    #bas gauche
+                    if(0<=case[0]+i<=nb_row_matrix and 0<=case[1]-diff<=nb_column_matrix):
+                        if(matrix[case[0]+i, case[1]-diff]=='.'):
+                            list_coordinates_empty.append((case[0]+i, case[1]-diff))
+                            matrix[case[0] + i, case[1] - diff]='#'
+
+                    #haut droite
+                    if (0 <= case[0] + diff <= nb_row_matrix and 0 <= case[1] + i <= nb_column_matrix):
+                        if (matrix[case[0] + diff, case[1] + i] == '.'):
+                            list_coordinates_empty.append((case[0] + diff, case[1] + i))
+                            matrix[case[0] + diff, case[1] + i] = '#'
+
+                    #haut gauche
+                    if (0 <= case[0] - diff <= nb_row_matrix and 0 <= case[1] + i <= nb_column_matrix):
+                        if (matrix[case[0] - diff, case[1] + i] == '.'):
+                            list_coordinates_empty.append((case[0] - diff, case[1] + i))
+                            matrix[case[0] - diff, case[1] + i] = '#'
+        print(list_coordinates_empty)
+        print(len(list_coordinates_empty))
--- testClasses.py
+++ testClasses.py
@@ -7,28 +7,32 @@ from model.Residential import Residential
 
 #definition de la ville
 matriceVille = np.array(
-    [['.', '.', '.','.','.','.','.','.','.','.'], ['3', '3', '3', '3', '.', '4', '4', '4', '4', '4'], ['.', '.', '.','.','.','4','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']])
+    [['.', '.', '.','.','.','.','.','.','.','.'], ['3', '3', '3', '3', '.', '4', '4', '4', '4', '4'], ['.', '.', '.','.','.','4','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.'],['.', '.', '.','.','.','.','.','.','.','.']])
 c = CityPlan(matriceVille, "Lyon", 4)
-print(c.matrix)
+#print(c.matrix)
 
  # definition des batiments
 matBuild = np.array([['.', '1', '.'], ['1', '1', '.'], ['1', '.', '1']])
 resi = Residential(0,matBuild, 25)
 
-print("residential placeable en 1,3 ? ",resi.is_placeable(c.matrix,1,3))
-print("residential placeable en 3,1 ? ",resi.is_placeable(c.matrix,3,1))
-print("residential placeable en 1,4 ? ",resi.is_placeable(c.matrix,1,4))
+#print("residential placeable en 1,3 ? ",resi.is_placeable(c.matrix,1,3))
+#print("residential placeable en 3,1 ? ",resi.is_placeable(c.matrix,3,1))
+#print("residential placeable en 1,4 ? ",resi.is_placeable(c.matrix,1,4))
 print("ajout residential en 1,3 ",c.add(resi, 1, 3))
 
 print("\n")
 matBuild2 = np.array([['.', '2', '.'], ['2', '2', '.'], ['2', '.', '2']])
 
 utili = Utility(0,matBuild2, 5)
-print(utili.matrix)
-print("\n")
+#print(utili.matrix)
+#print("\n")
 
 #ajout d'un utilitaire dans la ville
-print("ajout utili")
-print(c.add(utili, 3,1))
+#print("ajout utili")
+c.add(utili, 3,1)
+
+
+#utili.get_manhattan_surface(2,c.matrix)
+resi.get_manhattan_surface(2,c.matrix)
 print(c.matrix)
 
